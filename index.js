5f6c3ef8f825b3a4caedb4eb33bba44cbcfa1774
RetrossCZ <98403159+RetrossCZ@users.noreply.github.com>
2022-02-11 20:56:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
-let firstNumber = +prompt("Číslo1");
-let secondNumber = +prompt("Číslo2");
-let sign = prompt("Znak");
+let firstNumber = +prompt("Number2");
+let secondNumber = +prompt("Number2");
+let sign = prompt("Sign");
 if (sign === "+") {
   console.log(firstNumber + secondNumber);
 } else if (sign === "-") {
   console.log(firstNumber - secondNumber);
 } else if (sign === "*") {
   console.log(firstNumber * secondNumber);
-} else if (sign === "/" && secondNumber != 0) {
+} else if (sign === "/" && secondNumber !== 0) {
   console.log(firstNumber / secondNumber);
-} else sign === "/" && secondNumber === 0;
-{
-  alert("Toto nedokážu vypočítat!");
-}
-
-var firstNumber = Number(prompt("Zadej první číslo:"));
-var secondNumber = Number(prompt("Zadej druhé číslo:"));
-var sign = prompt("Zadej znak pro matematickou operaci");
-
-if (sign == "+") {
-  console.log(firstNumber + secondNumber);
-} else if (sign == "-") {
-  console.log(firstNumber - secondNumber);
-} else if (sign == "*") {
-  console.log(firstNumber * secondNumber);
-} else if (sign == "/" && secondNumber != 0) {
-  console.log(firstNumber / secondNumber);
-} else if (sign == "/" && secondNumber == 0) {
-  console.log("Nesmíš dělit nulou. Druhé číslo nesmí být nula při dělení!!!!");
-} else {
-  console.log("Nedokáži takovouto operaci provést.");
+} else if (sign === "/" && secondNumber === 0) {
+  alert("Wrong input.");
 }
